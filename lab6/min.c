e2e16001fd763a964accd63a897080c8bd2d3de7
zhuyan-9658 <1398131736@qq.com>
2019-12-27 01:19:49 -0800

    added a min.c file

--- lab6/min.c
+++ lab6/min.c
@@ -6,16 +6,16 @@ int main(int argc,char *argv[])
     int i=1;int j=0;
     int m=0; 
     double a[100];
-    double temp;
     for(i=1;i<argc;i++,j++){
     a[j]=atof(argv[i]);
     }
+    double min =a[1];
     for(m=0;m<argc-2;m++){
-        if(a[m]<a[m+1]){
-            temp=a[m]; a[m]=a[m+1];a[m+1]=temp;
+        if(min>a[m+1]){
+            min=a[m+1];
         }
     }
-    printf("The min is%g\n",temp);
+    printf("The min is%g\n",min);
     return 0;
 }
 
